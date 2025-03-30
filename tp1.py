32eef2cae7c340a82ede4e9c0b6d05eb592f26c0
SantiagoPittella <pittellasantiago@gmail.com>
2025-03-30 18:52:17 -0300

    add expected results check

--- tp1.py
+++ tp1.py
@@ -71,32 +71,3 @@ def buscar_culpable2(t, s):
     if len(coincidencias) != len(s):
         return (False, None)
     return (True, coincidencias)
-
-def parsear_archivo(archivo):
-    # Descarto primer linea (comentario)
-    # Segunda linea me da el largo de t y s, llamemoslo N.
-    # Itero N veces creando los t.
-    # Itero N veces creando los s.
-    with open(archivo) as archivo:
-        lineas = archivo.readlines()
-        n = int(lineas[1])
-        t = []
-        s = []
-        for i in range(n):
-            ti = lineas[2 + i]
-            [ti, ei] = ti.split(",")
-            t.append((int(ti), int(ei)))
-            s.append(int(lineas[2 + n + i]))
-        return t, s
-
-def pruebas_catedra():
-    import os
-
-    archivos = os.listdir(DIRECTORIO_PRUEBAS)
-
-    for archivo in archivos:
-        t, s = parsear_archivo(DIRECTORIO_PRUEBAS + archivo)
-        print(archivo)
-        print(buscar_culpable2(t, s))
-
-pruebas_catedra()
